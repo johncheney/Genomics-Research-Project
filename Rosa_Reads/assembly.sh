334af7703becabee7438e4ca669b933518edbd7c
johncheney <71104613+johncheney@users.noreply.github.com>
2021-02-04 01:49:38 +0900

    pipeline SUCCESS

--- Rosa_Reads/assembly.sh
+++ Rosa_Reads/assembly.sh
@@ -1,10 +1,10 @@
 #!bin/bash/
 
-#fastqc "$1, $2"  
+fastqc "$1, $2"  
 
-#bbduk.sh -Xmx24g in1="$1" in2="$2" out1="$1"_clean1.fastq out2="$2"_clean2.fastq ref=adapters ktrim=r k=23 mink=11 hdist=1 tpe tbo
+bbduk.sh -Xmx24g in1="$1" in2="$2" out1="$1"_clean1.fastq out2="$2"_clean2.fastq ref=adapters ktrim=r k=23 mink=11 hdist=1 tpe tbo
 
-#fastqc *_clean1.fastq *_clean2.fastq 
+fastqc *_clean1.fastq *_clean2.fastq 
 
 mkdir spades_output
 
