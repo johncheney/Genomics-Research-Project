6b76f0c676646573c7c2bfc2147f0632faf6a9be
johncheney <71104613+johncheney@users.noreply.github.com>
2021-02-05 16:12:43 +0900

    Update Transcriptome-Analysis.py

--- Transcriptome-Analysis.py
+++ Transcriptome-Analysis.py
@@ -1,12 +1,27 @@
 
 #https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4742321/
 
-Routine RNA-seq workflow may consist of the following five steps as shown in Fig. 
-1: (1) preprocessing of raw data, 
+#Analysis of Whole Transcriptome Sequencing Data: Workflow and Software
+# In Seok Yang and Sangwoo Kim
+'''
+Routine RNA-seq workflow may consist of the following five steps as shown in Fig. 1:
+ 
+  (1) preprocessing of raw data, 
+      Raw data QC: FASTQC
+      Read trimming: FASTX-Toolkit
   (2) read alignment,
+      Unspliced Aligners: BWA, Bowtie. 
+      Spliced Aligners: TopHat
   (3) transcriptome reconstruction, 
-  (4) expression quantification, and 
+      Reference Guided: Cufflinks, Scriptome
+      Reference Independent: Trinity, Oases
+  (4) expression quantification, 
+      Gene-level: ALEXA-seq, ERANGE
+      Inform-level: Cufflinks, Stringtie, Sailfish, 
   (5) differential expression analysis. 
+      Gene-level: NOIseq, EdgeR, DESeq, 
+      Isoform-level: Cuffdiff, EBSeq, Ballgown
+  
   As an initial step, RNA-seq data may be subjected to quality control (QC) of the raw data before data analysis.
   Similar to whole genome or exome sequencing, read alignment can be performed to map the reads to the reference genome or transcriptome. 
   Clinical samples including formalin-fixed paraffin-embedded specimen and cancer tissue biopsies are often degraded or exist in limited amount [6]. 
@@ -17,3 +32,4 @@ Routine RNA-seq workflow may consist of the following five steps as shown in Fig
   Finally, differential expression analysis is conducted using currently available programs. 
   In this review, we discuss the RNA-seq workflow and its related bioinformatics tools in each step (Table 1), focusing on transcriptome reconstruction and abundance 
   quantification.
+'''
